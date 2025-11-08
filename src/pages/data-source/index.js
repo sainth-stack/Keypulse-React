d6e77477bfb442a133be97c489f15f9e6c243bc4
sainath-stylishop <sainath@stylishop.com>
2025-11-08 18:58:36 +0530

    updatec dhanges

--- src/pages/data-source/index.js
+++ src/pages/data-source/index.js
@@ -761,7 +761,7 @@ export default function DataSource() {
             onChange={handleFileChange}
             disabled={isUploading}
             multiple 
-            accept=".csv,.xml" 
+            accept=".csv,.xml,.pdf" 
           />
           <div className="thumbnail-wrapper">
             <FaPlus className="plus-icon" />
